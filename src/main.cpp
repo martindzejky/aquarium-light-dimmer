8f3b84b719bc4f346a6fc5a70209b7beaec8350c
Martin Jakubik <martin@jakubik.info>
2020-10-30 17:40:43 +0100

    add a makefile

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+// TIME INTERVAL SETTINGS
+
+// TODO: these are the test numbers, update once in production
+#define MIN_TRANSITION_S 2
+#define MAX_TRANSITION_S 10
+#define DAY_LENGTH_S 60 * 60 * 24
+
+// PINS
+
+#define PIN_ALARM_INT 2
+#define PIN_RESET_BTN 3
+#define PIN_ERROR_LED 4
+#define PIN_MOSFET 11
+#define PIN_SDA A4
+#define PIN_SCL A5
+#define PIN_GRAD_POT A2
+
+
+// SETUP
+
+void setup() {}
+
+// LOOP
+
+void loop() {}
+
